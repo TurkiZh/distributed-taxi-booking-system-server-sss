2eca7d93b88f91d516231af4c06bb8d834264b3c
Robert Northard <robertnorthard@googlemail.com>
2016-01-31 02:41:32 +0000

    Map utils to use static methods.

--- src/main/java/com/robertnorthard/dtbs/server/layer/utils/geocoding/MapUtils.java
+++ src/main/java/com/robertnorthard/dtbs/server/layer/utils/geocoding/MapUtils.java
@@ -6,30 +6,13 @@ package com.robertnorthard.dtbs.server.layer.utils.geocoding;
  */
 public class MapUtils {
     
-    private static MapUtils singleton;
-    
-    /**
-     * private constructor as singleton class.
-     */
-    private MapUtils() { }
-    
-    public static MapUtils getInstance(){
-        if(singleton == null){
-            synchronized(MapUtils.class){
-                MapUtils.singleton = new MapUtils();
-            }
-        }
-        
-        return MapUtils.singleton;
-    }
-    
     /**
      * Validates provided latitude and longitude.
      * @param latitude
      * @param longitude
      * @return true if valid else false.
      */
-    public boolean validateCoordinates(double latitude, double longitude){
+    public static boolean validateCoordinates(double latitude, double longitude){
         
         if(latitude > 90 || latitude < -90){
             return false;
