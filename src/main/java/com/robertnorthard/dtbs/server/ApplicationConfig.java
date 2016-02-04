c2ab67ee4b51042f8d2425797cfa62939f6894b0
Robert Northard <robertnorthard@googlemail.com>
2016-02-04 18:15:22 +0000

    Enabled role based access control on RESTful resources.

--- src/main/java/com/robertnorthard/dtbs/server/ApplicationConfig.java
+++ src/main/java/com/robertnorthard/dtbs/server/ApplicationConfig.java
@@ -12,28 +12,30 @@ import org.glassfish.jersey.server.filter.RolesAllowedDynamicFeature;
 
 /**
  * Application configuration.
+ *
  * @author robertnorthard
- **/
+ *
+ */
 @ApplicationPath("/api")
 public class ApplicationConfig extends ResourceConfig {
-    public ApplicationConfig(){
-        
+
+    public ApplicationConfig() {
+
         // RESTful Controllers
-      //  register(AccountController.class);
-      //  register(AuthenticationController.class);
-      //  register(GeocodeController.class);
-      //  register(BookingController.class); 
-      //  register(TaxiController.class);
-        
-        
+        register(AccountController.class);
+        register(AuthenticationController.class);
+        register(GeocodeController.class);
+        register(BookingController.class);
+        register(TaxiController.class);
+
         // Filters        
         /* 
-        In order to use security annotations (package javax.annotation.security) 
-        to restrict access to your resources you need to register RolesAllowedDynamicFeature.
-        Source: http://stackoverflow.com/questions/21536321/securitycontext-doesnt-work-with-rolesallowed
-        */
-      //  register(RolesAllowedDynamicFeature.class);
-      //  register(AuthenticationFilter.class);
-      
+         In order to use security annotations (package javax.annotation.security) 
+         to restrict access to your resources you need to register RolesAllowedDynamicFeature.
+         Source: http://stackoverflow.com/questions/21536321/securitycontext-doesnt-work-with-rolesallowed
+         */
+        register(RolesAllowedDynamicFeature.class);
+        register(AuthenticationFilter.class);
+
     }
 }
