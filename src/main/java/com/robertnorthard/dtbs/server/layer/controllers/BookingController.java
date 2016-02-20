e2e559a12105612ad0f107acb951839e18e49715
Robert Northard <robertnorthard@googlemail.com>
2016-02-20 19:05:07 +0000

    Updates should use PUT http request type methods.

--- src/main/java/com/robertnorthard/dtbs/server/layer/controllers/BookingController.java
+++ src/main/java/com/robertnorthard/dtbs/server/layer/controllers/BookingController.java
@@ -23,6 +23,7 @@ import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -204,7 +205,7 @@ public class BookingController {
      * @return the booking object. If the user is authenticated to view the
      * booking.
      */
-    @POST
+    @PUT
     @Path("/{id}/accept")
     @Produces(MediaType.APPLICATION_JSON)
     @RolesAllowed("driver")
@@ -241,7 +242,7 @@ public class BookingController {
      * @return the booking object. If the user is authenticated to view the
      * booking.
      */
-    @POST
+    @PUT
     @Path("/{id}/cancel")
     @Produces(MediaType.APPLICATION_JSON)
     @RolesAllowed("passenger")
